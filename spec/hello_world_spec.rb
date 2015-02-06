9aeae8f14bba1bc60572c4bc3f1757f848098481
adamjonas <jonas@flatironschool.com>
2015-02-06 18:05:26 -0500

    now failing

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -3,7 +3,7 @@ describe "Hello World" do
   it 'puts "Hello World!"' do
     # $stdout represents the output of your program, don't worry about how
     # we are testing this, focus on simply coding your solution.
-    false
+    expect(true).to be_falsey
     # Get your program to puts "Hello World!"
     # Code your solution right here.
   end
